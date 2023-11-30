14ab75dbe942757f79105f395da6ff5635a9c9b5
RodrigoCostaTA <rcosta@solvd.com>
2023-11-30 14:59:17 -0300

    [REFACTOR] migrated code from typeORM to mangoose
    updated Student entity
    Added basic test for CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,51 @@
-import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { Student } from './entities/student.entity';
+import mongoose from 'mongoose';
+import Student from './entities/Student';
 
-async function bootstrapConnection() {
-    try {
-        const connection = await createConnection();
-        console.log('Connected to MongoDB: ${connection.name');
-
-        
-        const studentRepository = connection.getRepository(Student);
-
-        const newStudent = new Student();
-        newStudent.name = 'John Doe';
-        newStudent.age = 20;
-        
-        await studentRepository.save(newStudent);
-        
-        const foundStudent = await studentRepository.findOne({
-            where: { name: 'John Doe' },
-          });
-        console.log('Found student:', foundStudent);
-
-    } catch (err) {
-        console.error('Error connecting to MongoDB: ', err);
-    }
+// Establish MongoDB connection
+async function connectToDatabase() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/tsPractice');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
 
-bootstrapConnection();
+async function testCRUDOperations() {
+  try {
+    // Create
+    const newStudent = new Student({
+      name: 'John Doe',
+      age: 20,
+      studentId: '12345',
+    });
+    await newStudent.save();
+
+    // Read
+    const foundStudent = await Student.findOne({ name: 'John Doe' });
+    console.log('Found student:', foundStudent);
+
+    // Update
+    if (foundStudent) {
+      foundStudent.age = 21;
+      await foundStudent.save();
+    }
+
+    // Delete
+    if (foundStudent) {
+        // Student found, proceed with deletion
+        const deletedStudent = await foundStudent.deleteOne();
+        console.log('Deleted student:', deletedStudent);
+    } else {
+        // Student not found, handle accordingly
+        console.error('Student not found');
+    }
+  } catch (error) {
+    console.error('Error performing CRUD operations:', error);
+  }
+}
+
+// Connect to the database and perform CRUD operations
+connectToDatabase().then(() => {
+  testCRUDOperations().finally(() => mongoose.disconnect());
+});
